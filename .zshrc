ffdad6c8d103a8373adf2aeb12036bd06c433d0a
Jorge Castellon Jr <jorge.castellon.jr@gmail.com>
2025-07-03 14:26:35 -0700

    Adds pretty terminal stuff;

--- .zshrc
+++ .zshrc
@@ -5,6 +5,10 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
+plugins=(
+  zsh-syntax-highlighting
+)
+
 export COPYFILE_DISABLE=1
 
 export BUN_INSTALL="$HOME/.bun" 
--- crucible/setup.sh
+++ crucible/setup.sh
@@ -8,8 +8,18 @@ brewPackages=(
   node
   sketchybar
   yazi
+  powerlevel10k
+  zsh-autosuggestions
+  starship
+  fzf
 )
 
 for package in ${brewPackages[@]}; do
   brew install ${package}
 done
+
+echo "source $(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme" >>~/.config/.zshrc
+source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  
