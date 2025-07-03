6d15fd65f8bb4ac01cac60f8d048b5917755218b
Jorge Castellon Jr <jorge.castellon.jr@gmail.com>
2025-07-03 14:33:58 -0700

    removes folder before installing zsh syntax highliting

--- crucible/setup.sh
+++ crucible/setup.sh
@@ -24,5 +24,6 @@ cp ~/.config/.p10k.zsh ~/.p10k.zsh
 echo "source $(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme" >>~/.config/.zshrc
 source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 
+rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
   
